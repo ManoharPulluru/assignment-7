b90a272002b27aa1521f2d336550991bf7a0aeca
ManoharPulluru <125332698+ManoharPulluru@users.noreply.github.com>
2023-06-05 11:16:07 +0530

    first commit

---
+++ initialData.js
@@ -0,0 +1,22 @@
+const studentData = [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "currentClass": "10",
+      "division": "A"
+    },
+    {
+      "id": 2,
+      "name": "Jane Smith",
+      "currentClass": "9",
+      "division": "B"
+    },
+    {
+      "id": 3,
+      "name": "Adam Johnson",
+      "currentClass": "11",
+      "division": "C"
+    }
+  ];
+  module.exports = studentData;
+  
---
+++ model/userModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const createData = new mongoose.Schema({
+    name:String,
+    currentClass:String,
+    division:String
+    
+});
+module.exports = mongoose.model('collection1',createData);
